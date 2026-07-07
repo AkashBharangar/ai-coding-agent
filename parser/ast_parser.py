fa36881af58b5618a50dd75af1098e8ba7119844
Akash ‎ <kingakash10@gmail.com>
2026-07-07 09:56:32 +0530

    added ast parser

---
+++ parser/ast_parser.py
@@ -0,0 +1,31 @@
+import ast
+
+
+def parse_python(code: str):
+
+    tree = ast.parse(code)
+
+    result = {
+        "functions": [],
+        "classes": [],
+        "imports": [],
+    }
+
+    for node in ast.walk(tree):
+
+        if isinstance(node, ast.FunctionDef):
+            result["functions"].append(node.name)
+
+        elif isinstance(node, ast.ClassDef):
+            result["classes"].append(node.name)
+
+        elif isinstance(node, ast.Import):
+
+            for alias in node.names:
+                result["imports"].append(alias.name)
+
+        elif isinstance(node, ast.ImportFrom):
+
+            result["imports"].append(node.module)
+
+    return result
